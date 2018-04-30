15c162c4827fe633cb14c04e1e3202fe44e6fce5
Tsun-Yi Yang <shamangary@hotmail.com>
2018-04-30 21:49:47 +0800

    Add files via upload

---
+++ training_and_testing/plot.sh
@@ -0,0 +1,5 @@
+python plot_reg.py --input mobilenet_reg_0.25_64/history_mobilenet_reg_0.25_64.h5
+python plot_reg.py --input mobilenet_reg_0.5_64/history_mobilenet_reg_0.5_64.h5
+
+python plot_reg.py --input densenet_reg_13_64/history_densenet_reg_13_64.h5
+python plot_reg.py --input densenet_reg_19_64/history_densenet_reg_19_64.h5
---
+++ training_and_testing/ssrnet_plot.sh
@@ -0,0 +1,29 @@
+python plot_reg.py --input ssrnet_3_3_3_64_0.0_0.0/history_ssrnet_3_3_3_64_0.0_0.0.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.0_0.25/history_ssrnet_3_3_3_64_0.0_0.25.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.0_0.5/history_ssrnet_3_3_3_64_0.0_0.5.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.0_0.75/history_ssrnet_3_3_3_64_0.0_0.75.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.0_1.0/history_ssrnet_3_3_3_64_0.0_1.0.h5
+
+python plot_reg.py --input ssrnet_3_3_3_64_0.25_0.0/history_ssrnet_3_3_3_64_0.25_0.0.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.25_0.25/history_ssrnet_3_3_3_64_0.25_0.25.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.25_0.5/history_ssrnet_3_3_3_64_0.25_0.5.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.25_0.75/history_ssrnet_3_3_3_64_0.25_0.75.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.25_1.0/history_ssrnet_3_3_3_64_0.25_1.0.h5
+
+python plot_reg.py --input ssrnet_3_3_3_64_0.5_0.0/history_ssrnet_3_3_3_64_0.5_0.0.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.5_0.25/history_ssrnet_3_3_3_64_0.5_0.25.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.5_0.5/history_ssrnet_3_3_3_64_0.5_0.5.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.5_0.75/history_ssrnet_3_3_3_64_0.5_0.75.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.5_1.0/history_ssrnet_3_3_3_64_0.5_1.0.h5
+
+python plot_reg.py --input ssrnet_3_3_3_64_0.75_0.0/history_ssrnet_3_3_3_64_0.75_0.0.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.75_0.25/history_ssrnet_3_3_3_64_0.75_0.25.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.75_0.5/history_ssrnet_3_3_3_64_0.75_0.5.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.75_0.75/history_ssrnet_3_3_3_64_0.75_0.75.h5
+python plot_reg.py --input ssrnet_3_3_3_64_0.75_1.0/history_ssrnet_3_3_3_64_0.75_1.0.h5
+
+python plot_reg.py --input ssrnet_3_3_3_64_1.0_0.0/history_ssrnet_3_3_3_64_1.0_0.0.h5
+python plot_reg.py --input ssrnet_3_3_3_64_1.0_0.25/history_ssrnet_3_3_3_64_1.0_0.25.h5
+python plot_reg.py --input ssrnet_3_3_3_64_1.0_0.5/history_ssrnet_3_3_3_64_1.0_0.5.h5
+python plot_reg.py --input ssrnet_3_3_3_64_1.0_0.75/history_ssrnet_3_3_3_64_1.0_0.75.h5
+python plot_reg.py --input ssrnet_3_3_3_64_1.0_1.0/history_ssrnet_3_3_3_64_1.0_1.0.h5
