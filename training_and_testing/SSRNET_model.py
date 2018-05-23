2dcbb68bc0fe450f73b8fa8808a59f098a0b56c9
Tsun-Yi Yang <shamangary@hotmail.com>
2018-05-23 10:28:13 +0800

    Update SSRNET_model.py

--- training_and_testing/SSRNET_model.py
+++ training_and_testing/SSRNET_model.py
@@ -152,15 +152,15 @@ class SSR_net:
 
             for i in range(0,s1):
                 a = a+(i+lambda_local*x[6][:,i])*x[0][:,i]
-            a = a/(s1*(1+lambda_d*x[3][:,0]))
+            a = a/(s1*(1+lambda_d*x[3]))
 
             for j in range(0,s2):
                 b = b+(j+lambda_local*x[7][:,j])*x[1][:,j]
-            b = b/(s1*(1+lambda_d*x[3][:,0]))/(s2*(1+lambda_d*x[4][:,0]))
+            b = b/(s1*(1+lambda_d*x[3]))/(s2*(1+lambda_d*x[4]))
 
             for k in range(0,s3):
                 c = c+(k+lambda_local*x[8][:,k])*x[2][:,k]
-            c = c/(s1*(1+lambda_d*x[3][:,0]))/(s2*(1+lambda_d*x[4][:,0]))/(s3*(1+lambda_d*x[5][:,0]))
+            c = c/(s1*(1+lambda_d*x[3]))/(s2*(1+lambda_d*x[4]))/(s3*(1+lambda_d*x[5]))
 
 
             a = (a+b+c)*V
