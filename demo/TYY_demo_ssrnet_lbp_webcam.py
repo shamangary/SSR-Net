096da715c200cc80ad23f67b8d964c156b9591c0
Tsun-Yi Yang <shamangary@hotmail.com>
2018-06-21 17:41:28 +0800

    Add files via upload

--- demo/TYY_demo_ssrnet_lbp_webcam.py
+++ demo/TYY_demo_ssrnet_lbp_webcam.py
@@ -2,7 +2,7 @@ import os
 import cv2
 import numpy as np
 import argparse
-from SSRNET_model import SSR_net
+from SSRNET_model import SSR_net, SSR_net_general
 import sys
 import timeit
 from moviepy.editor import *
@@ -15,7 +15,7 @@ def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
     cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
     cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness)
 
-def draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_detection,time_network,time_plot):
+def draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,model_gender,time_detection,time_network,time_plot):
     
     #for i, d in enumerate(detected):
     for i, (x,y,w,h) in enumerate(detected):
@@ -31,19 +31,20 @@ def draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_det
         xw2 = min(int(x2 + ad * w), img_w - 1)
         yw2 = min(int(y2 + ad * h), img_h - 1)
         
+        faces[i,:,:,:] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+        
+        faces[i,:,:,:] = cv2.normalize(faces[i,:,:,:], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
         cv2.rectangle(input_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
         cv2.rectangle(input_img, (xw1, yw1), (xw2, yw2), (0, 0, 255), 2)
-        faces[i,:,:,:] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
-    
+        
     
     start_time = timeit.default_timer()
     if len(detected) > 0:
         # predict ages and genders of the detected faces
-        results = model.predict(faces)
-        predicted_ages = results
-
-        #predicted_ages = np.round(predicted_ages)
+        predicted_ages = model.predict(faces)
+        predicted_genders = model_gender.predict(faces)
+        
 
     # draw results
     for i, (x,y,w,h) in enumerate(detected):
@@ -53,7 +54,12 @@ def draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_det
         y1 = y
         x2 = x+w
         y2 = y+h
-        label = "{}".format(int(predicted_ages[i]))
+
+        gender_str = 'male'
+        if predicted_genders[i]<0.5:
+            gender_str = 'female'
+
+        label = "{},{}".format(int(predicted_ages[i]),gender_str)
         
         draw_label(input_img, (x1, y1), label)
     
@@ -76,6 +82,8 @@ def draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_det
 def main():
     
     weight_file = "../pre-trained/morph2/ssrnet_3_3_3_64_1.0_1.0/ssrnet_3_3_3_64_1.0_1.0.h5"
+    weight_file_gender = "../pre-trained/wiki_gender_models/ssrnet_3_3_3_64_1.0_1.0/ssrnet_3_3_3_64_1.0_1.0.h5"
+    
     face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
     try:
         os.mkdir('./img')
@@ -90,11 +98,13 @@ def main():
     model = SSR_net(img_size,stage_num, lambda_local, lambda_d)()
     model.load_weights(weight_file)
 
+    model_gender = SSR_net_general(img_size,stage_num, lambda_local, lambda_d)()
+    model_gender.load_weights(weight_file_gender)
     
     # capture video
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1024*0.5)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768*0.5)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1024*1)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768*1)
     
     
 
@@ -104,7 +114,7 @@ def main():
     time_network = 0
     time_plot = 0
     skip_frame = 5 # every 5 frame do 1 detection and network forward propagation
-    ad = 0.9
+    ad = 0.5
 
     while True:
         # get video frame
@@ -128,11 +138,11 @@ def main():
             faces = np.empty((len(detected), img_size, img_size, 3))
 
             
-            input_img,time_network,time_plot = draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_detection,time_network,time_plot)
+            input_img,time_network,time_plot = draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,model_gender,time_detection,time_network,time_plot)
             cv2.imwrite('img/'+str(img_idx)+'.png',input_img)
             
         else:
-            input_img,time_network,time_plot = draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,time_detection,time_network,time_plot)
+            input_img,time_network,time_plot = draw_results(detected,input_img,faces,ad,img_size,img_w,img_h,model,model_gender,time_detection,time_network,time_plot)
         
         #Show the time cost (fps)
         print('avefps_time_detection:',1/time_detection)
